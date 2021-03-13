88fa14c38d82b7d4f3ae662bee70e39bf37c206d
Shawn Paulsen <shawnpaulsen@Shawns-MacBook-Pro.local>
2021-03-13 16:15:40 -0700

    doodling with page

--- Assets/script.js
+++ Assets/script.js
@@ -2,10 +2,29 @@ console.log("hello");
 
 //need event listener for timer to start with button- start.addEventlistener("click", startQuiz);
 
-// Questions with 4 answers- list correct or incorrect- if/then
+//class= "start_btn"
+
+// Questions with 4 answers- list correct or incorrect- if/then  -then trigger next
 
 //need event listener for incoorect answer - ...addEventListener("click", incorrect);
 
 //need event listener for start again  - ...addEventListener("click", start again);
 
 //need event listener for clear highscores - ...addEventListener("click", clear highscores);
+
+//event.target- targets value
+
+//generate question function
+
+//a bunch of small functions
+
+//timer  w/ decrement
+
+// get question
+
+//console.log everything - can set alerts to help troubleshoot
+
+//var x: document.getElementById
+
+
+
